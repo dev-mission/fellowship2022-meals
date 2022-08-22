e8b2c974b71237eff1796c2deb140799e5005263
Jackson Tran <javtran@ucsc.edu>
2022-08-21 20:17:20 -0700

    footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,14 +1,42 @@
 import './Footer.scss';
 function Footer() {
   return (
-    <div className="fixed-bottom footer">
-      <div>
-        <img className="footer-img" src={`${process.env.PUBLIC_URL}/footer-logo.png`} />
+    <div>
+      <div
+        className="contact"
+        style={{ background: `#f9f9f9 url(${process.env.PUBLIC_URL}/contact-bg.png) no-repeat`, backgroundPosition: 'right' }}>
+        <div className="heading">Contact Us</div>
+        <div className="information">
+          <div className="general-information">
+            General information: <span style={{ fontWeight: '400' }}>Call </span>
+            <span className="span-phone">
+              <i className="fa fa-phone fa-lg phone" aria-hidden="true"></i>
+              (417) 557-5000
+            </span>
+            .
+          </div>
+          <div className="general-information">
+            For faster Program assistance:{' '}
+            <span style={{ fontWeight: '400' }}>
+              Visit the{' '}
+              <span className="span-phone" style={{ fontWeight: 'bold' }}>
+                Contact
+              </span>{' '}
+              page to directly call the location you'd lick to reach.
+            </span>
+          </div>
+        </div>
       </div>
-      <h3>
-        ©2022 and County of San Francisco
-        <span>Human Services Agency</span>
-      </h3>
+
+      <div className="footer">
+        <div>
+          <img className="footer-img" src={`${process.env.PUBLIC_URL}/footer-logo.png`} />
+        </div>
+        <h3>
+          ©2022 and County of San Francisco
+          <span>Human Services Agency</span>
+        </h3>
+      </div>
     </div>
   );
 }
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,21 @@
+import './Home.scss';
+
 function Home() {
   return (
-    <main className="container">
-      <h1>Home</h1>
+    <main className="home">
+      <h1>SF Ready Meals</h1>
+      <div className="description">
+        SFHSA's Congregate Meals Program offers nutritious, low-cost meals to seniors 60+ and adults with disabilities everyday at many
+        community dining sites throughout the city. In response to COVID-19, most sites offer takeout meals.
+      </div>
+      <h2>Check Your Eligibility</h2>
+      <div className="survey-container">
+        <div className="question-text">Are you an adult with disabilities?</div>
+        <div className="button-container">
+          <button>Yes</button>
+          <button>No</button>
+        </div>
+      </div>
     </main>
   );
 }
