7d372d05cf085ff8c220fd173b5d724cef528958
Jackson Tran <javtran@ucsc.edu>
2022-11-02 14:21:46 -0700

    moved eligibility to its own component

---
+++ client/src/Eligibility/Eligibility.js
@@ -0,0 +1,14 @@
+function Eligibility() {
+  return (
+    <>
+      <h2>Check Your Eligibility</h2>
+      <div className="survey-container">
+        <div className="question-text">Are you an adult with disabilities?</div>
+        <div className="button-container">
+          <button>Yes</button>
+          <button>No</button>
+        </div>
+      </div>
+    </>
+  );
+}
--- client/src/Home.js
+++ client/src/Home.js
@@ -9,14 +9,6 @@ function Home() {
         SFHSA's Congregate Meals Program offers nutritious, low-cost meals to seniors 60+ and adults with disabilities everyday at many
         community dining sites throughout the city. In response to COVID-19, most sites offer takeout meals.
       </div>
-      <h2>Check Your Eligibility</h2>
-      <div className="survey-container">
-        <div className="question-text">Are you an adult with disabilities?</div>
-        <div className="button-container">
-          <button>Yes</button>
-          <button>No</button>
-        </div>
-      </div>
       <Sites />
     </main>
   );
